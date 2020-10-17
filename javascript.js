def7f8fa188ffb93c5ef21bbaa4c44c47861df56
Carolina Ramon <carolinaelizabeth.ramon@gmail.com>
2020-10-17 11:21:14 -0300

    Diferencias entre var let y const

--- javascript.js
+++ javascript.js
@@ -25,7 +25,7 @@ console.log(num2);
 
 var nombre = 'Elizabeth';
 
-//condicional:
+//CONDICIONAL:
 if(num>1700 || nombre == 'Carolina'){
     console.log('Verdadero');
 }else{
@@ -96,3 +96,56 @@ function suma(n1, n2){
 }
 
 suma(80,20);
+
+//DIFERENCIAS ENTRE var, let y const:
+
+//var edad = 55;
+//var edad = 60;//debilidad de var: estoy reescribiendo una variable
+
+//console.log(edad);
+
+let edad = 55;
+//let edad = 60;//con let esto no pasa: tira error si declaro la misma variable de nuevo
+
+edad = 100;//no hay problema con cambiarle el valor
+
+console.log(edad);
+
+for(var i=80;i<85;i++){
+    console.log(i);
+}
+
+console.log(i);//var i sigue viva fuera del ciclo for
+
+let j=34;//defino j fuera del ciclo for, en el ambiente/scope global
+
+for(let j=80;j<85;j++){//j definida dentro del ambiente/scope del ciclo for
+    console.log(j);
+}
+
+//console.log(j);//let j no se encuentra definida fuera del ciclo for
+
+console.log(j);
+
+//En resumen, var existe en cualquier ambiente, pero let no.
+
+var numero4;
+let numero5;
+//const numero6; //tira error porque las constantes se declaran y se inicializan al mismo tiempo. No puedo solo declarar const, tengo que asignarle un valor.
+
+console.log(numero4);
+console.log(numero5);
+
+const numero6 = 90;
+console.log(numero6);
+
+//las constantes no se pueden redeclarar ni se le puede asignar otro valor:
+//const numero6 = 78;//esto tira error
+//numero6 = 78;//esto tira error
+//const es un valor CONSTANTE
+
+//FUNCION PUSH:
+const numeros = [12,13,14,15];
+//numeros = [30];//esto tira error
+numeros.push(16);
+console.log(numeros);
